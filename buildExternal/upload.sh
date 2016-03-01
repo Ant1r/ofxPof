742c0db145ea8871965227220c8cc5c599061a02
Antoine Rousseau <_antoine_@metalu.net>
2016-03-01 10:05:38 +0100

    Add deken upload util for linux external project

---
+++ buildExternal/upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf pof/
+mkdir pof/
+cp -a ../help pof/
+if [ -n "`uname -a | grep x86_64`" ]
+	then cp bin/pof.pd_linux pof/pof.l_ia64
+	else cp bin/pof.pd_linux pof/pof.l_i386
+fi
+	
+POF_VERSION_MAJOR=`grep POF_VERSION_MAJOR ../src/version.h | cut -d' ' -f 3`
+POF_VERSION_MINOR=`grep POF_VERSION_MINOR ../src/version.h | cut -d' ' -f 3`
+POF_VERSION_PATCH=`grep POF_VERSION_PATCH ../src/version.h | cut -d' ' -f 3`
+
+deken package --version ${POF_VERSION_MAJOR}.${POF_VERSION_MINOR}.${POF_VERSION_PATCH} pof
+
+
