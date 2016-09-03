cffa10b179e9adbed2f096f3310c84ea926d6c4c
Antoine Rousseau <antoine@metalu.net>
2016-09-03 11:29:57 +0200

    add deken upload script

---
+++ OSXexternal/upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export PATH+=:~/bin/
+
+rm -rf pof/
+mkdir pof/
+cp -a ../help/* pof/
+#if [ -n "`uname -a | grep x86_64`" ]
+#	then cp bin/pof.pd_linux pof/pof.l_ia64
+#	else cp bin/pof.pd_linux pof/pof.l_i386
+#fi
+cp ~/Library/Pd/pof/pof.pd_darwin pof/
+cp ../../../export/osx/libs/libfmodex.dylib pof/
+	
+POF_VERSION_MAJOR=`grep POF_VERSION_MAJOR ../src/version.h | cut -d' ' -f 3`
+POF_VERSION_MINOR=`grep POF_VERSION_MINOR ../src/version.h | cut -d' ' -f 3`
+POF_VERSION_PATCH=`grep POF_VERSION_PATCH ../src/version.h | cut -d' ' -f 3`
+
+if [ x$1 == xtest ] ; then
+	deken package --version ${POF_VERSION_MAJOR}.${POF_VERSION_MINOR}.${POF_VERSION_PATCH} pof
+else
+	deken upload --version ${POF_VERSION_MAJOR}.${POF_VERSION_MINOR}.${POF_VERSION_PATCH} pof
+	rm -rf pof/
+fi
+
+
