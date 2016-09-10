15cce5501c6c11c6844e6b59d8675799608bc9b8
Antoine Rousseau <_antoine_@metalu.net>
2016-09-10 11:12:31 +0200

    copy depending libs to dist directory before packaging

--- buildExternal/upload.sh
+++ buildExternal/upload.sh
@@ -2,12 +2,27 @@
 
 rm -rf pof/
 mkdir pof/
+echo copying help patches...
 cp -a ../help/* pof/
 #if [ -n "`uname -a | grep x86_64`" ]
 #	then cp bin/pof.pd_linux pof/pof.l_ia64
 #	else cp bin/pof.pd_linux pof/pof.l_i386
 #fi
+echo copying pof external...
 cp bin/pof.* pof/
+
+mkdir pof/libs
+echo copying libs...
+cp -a bin/libs/* pof/libs
+
+LIBS_TO_COPY="freeimage boost_filesystem boost_system openal gstreamer gstbase gstapp gstvideo sndfile"
+
+for libtocopy in $LIBS_TO_COPY ; do 
+	libfile=`ldd bin/pof.* | grep lib${libtocopy} | cut -d' ' -f 3`
+	echo "$libtocopy : copying $libfile in libs"
+	cp $libfile pof/libs
+	done
+
 	
 POF_VERSION_MAJOR=`grep POF_VERSION_MAJOR ../src/version.h | cut -d' ' -f 3`
 POF_VERSION_MINOR=`grep POF_VERSION_MINOR ../src/version.h | cut -d' ' -f 3`
