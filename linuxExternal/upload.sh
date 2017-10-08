ded32373e4e57f329c81a94f66f684c9d91b3341
Antoine Rousseau <_antoine_@metalu.net>
2017-10-08 22:35:15 +0200

    update Linux distribution libs list

--- linuxExternal/upload.sh
+++ linuxExternal/upload.sh
@@ -24,7 +24,9 @@ cp -a bin/libs/* pof/libs
 LIBS_TO_COPY="freeimage boost_filesystem boost_system openal gstreamer gstbase gstapp gstvideo \
 	sndfile GLEW openjpeg IlmImf IlmThread Half Iex raw gpg-error gcrypt datrie graphite2 \
 	protobuf-lite mirprotobuf mircommon jbig lzma gomp lcms2 jasper jpegxr Xdmcp Xau orc \
-	jxrglue pcre mirclient atspi wayland-client wayland-egl bwayland-cursor Xfixes openjp2 png webp webpmux"
+	jxrglue pcre mirclient atspi wayland-client wayland-egl wayland-cursor Xfixes \
+	openjp2 jpeg png webp webpmux \
+	"
 
 #	gstadaptivedemux gstallocators gstbadbase libgstbadvideo gstaudio gstbadaudio gstcheck gstcodecparsers gstcontroller gstfft gstgl gstinsertbin gstmpegts gstnet gstpbutils gstphotography gstplayer gstriff gstrtp gstrtsp gsttag
 
