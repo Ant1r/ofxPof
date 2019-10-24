aca2d95e7f8f6cc076401768547384e6fa7eca51
Antoine Rousseau <_antoine_@metalu.net>
2019-10-24 18:11:25 +0200

    pofvisible: new 'once' mode

--- src/pofVisible.cc
+++ src/pofVisible.cc
@@ -7,6 +7,8 @@
 #include "pofLayer.h"
 
 t_class *pofvisible_class;
+bool pofVisible::FORCE_ONCE = false;
+std::list<pofVisible*> pofVisible::onces;
 
 static void pofvisible_float(void *x, t_float t);
 
@@ -14,27 +16,35 @@ static void *pofvisible_new(t_symbol *sym,int argc, t_atom *argv)
 {
 	bool visible = false;
 	bool forceTouchable = false;
+	bool once = false;
+	bool continuousForce = false;
 	t_symbol *layer = NULL;
  
-	pofVisible* obj = new pofVisible(pofvisible_class,visible);
 
-	if(argc && argv->a_type == A_SYMBOL) {
-		layer = atom_getsymbol(argv); argc--; argv++;
-		visible = 1; // visible defaults to 1 when layer is given		
-		if(argc && argv->a_type == A_FLOAT) {
-			visible = atom_getfloat(argv); argc--; argv++;
-			if(argc && argv->a_type == A_FLOAT) forceTouchable = atom_getfloat(argv) != 0;
+	while(argc) {
+		if(argv->a_type == A_SYMBOL) {
+			t_symbol *argsym = atom_getsymbol(argv);
+			if(argsym == gensym("-once")) {
+				once= true;
+			} else if(argsym == gensym("-continuousForce")) {
+				once= true;
+				continuousForce = true;
+			}
+			else {
+				layer = atom_getsymbol(argv);
+				visible = 1; // visible defaults to 1 when layer is given
+			}
+		} else if(argv->a_type == A_FLOAT) {
+			visible = atom_getfloat(argv);
+			if((argc > 1) && (argv + 1)->a_type == A_FLOAT) {
+				forceTouchable = atom_getfloat(argv + 1) != 0;
+				argc--; argv++;
+			}
 		}
-	} else if(argc && argv->a_type == A_FLOAT) {
-		visible = atom_getfloat(argv); argc--; argv++;
-		if(argc && argv->a_type == A_FLOAT) {
-			forceTouchable = atom_getfloat(argv) != 0;
-			argc--; argv++;
-		}
-		if(argc && argv->a_type == A_SYMBOL) layer = atom_getsymbol(argv);
+		argc--; argv++;
 	}
 
-	obj->forceTouchable = forceTouchable;
+	pofVisible* obj = new pofVisible(pofvisible_class,visible, forceTouchable, once, continuousForce);
 	
 	pofvisible_float((void*) (obj->pdobj), visible);
 	obj->layer = layer;
@@ -49,15 +59,26 @@ static void pofvisible_free(void *x)
 static void pofvisible_float(void *x, t_float t)
 {
 	pofVisible *px = (pofVisible*)(((PdObject*)x)->parent);
-	
-	px->visible = t!=0;
-	px->setTouchable(px->visible || px->forceTouchable);
+
+	px->setTouchable((t != 0) || px->forceTouchable);
+	px->visible = (t != 0);
+}
+
+static void pofvisible_continuousForce(void *x, t_float t)
+{
+	pofVisible *px = (pofVisible*)(((PdObject*)x)->parent);
+	px->continuousForce = (t != 0);
+}
+
+static void pofvisible_force(void *x)
+{
+	pofVisible *px = (pofVisible*)(((PdObject*)x)->parent);
+	px->force = true;
 }
 
 static void pofvisible_forceTouchable(void *x, t_float t)
 {
 	pofVisible *px = (pofVisible*)(((PdObject*)x)->parent);
-	
 	px->forceTouchable = t!=0;
 	px->setTouchable(px->visible || px->forceTouchable);
 }
@@ -82,13 +103,59 @@ void pofVisible::setup(void)
 	POF_SETUP(pofvisible_class);
 	class_addfloat(pofvisible_class, (t_method)pofvisible_float);
 	class_addmethod(pofvisible_class, (t_method)pofvisible_forceTouchable, gensym("forceTouchable"), A_DEFFLOAT, A_NULL);
+	class_addmethod(pofvisible_class, (t_method)pofvisible_force, gensym("force"), A_NULL);
+	class_addmethod(pofvisible_class, (t_method)pofvisible_continuousForce, gensym("continuousForce"), A_DEFFLOAT, A_NULL);
 	class_addmethod(pofvisible_class, (t_method)pofvisible_layer, gensym("layer"), A_DEFSYM, A_NULL);
 	class_addmethod(pofvisible_class, (t_method)pofvisible_nolayer, gensym("nolayer"), A_NULL);
+	ofAddListener(pofBase::initFrameEvent, &pofVisible::initFrame);
+}
+
+pofVisible::pofVisible(t_class *Class, bool v0, bool fT0, bool once0, bool continuousForce0):
+	pofTouchable(Class, v0),visible(v0), forceTouchable(fT0), once(once0), continuousForce(continuousForce0), layer(NULL)
+{
+	if(once) onces.push_back(this);
+}
+
+pofVisible::~pofVisible()
+{
+	if(once) onces.remove(this);
 }
 
 void pofVisible::tree_draw()
 {
-	if( visible && ((layer == NULL) || (layer == pofLayer::currentLayer)) )
-		pofBase::tree_draw();
+	bool tmp_force_once = FORCE_ONCE;
+	bool doDraw = false;
+	
+	if(once) {
+		if(final_force) {
+			FORCE_ONCE = true;
+			final_force = false;
+		}
+		if(final_once || FORCE_ONCE) doDraw = 1;
+		final_once = false;
+	} else doDraw = visible;
+	
+	if(doDraw && layer) doDraw = (layer == pofLayer::currentLayer);
+	
+	if(doDraw) pofBase::tree_draw();
+	
+	FORCE_ONCE = tmp_force_once;
 }
 
+void pofVisible::initFrame(ofEventArgs & args)
+{
+	std::list<pofVisible*>::iterator it = onces.begin();
+	while(it != onces.end()) {
+		if((*it)->once) {
+			if((*it)->force || (*it)->continuousForce) {
+				(*it)->final_force = true;
+				(*it)->force = false;
+			} else if((*it)->visible) {
+				(*it)->final_once = true;
+				(*it)->visible = false;
+			}
+		}
+		it++;
+	}
+	FORCE_ONCE = false;
+}
--- src/pofVisible.h
+++ src/pofVisible.h
@@ -11,17 +11,24 @@ class pofVisible;
 
 class pofVisible: public pofTouchable {
 	public:
-		pofVisible(t_class *Class, bool v0 = false, bool fT0 = false):
-			pofTouchable(Class, v0),visible(v0), forceTouchable(fT0), layer(NULL) 
-			{}
+		pofVisible(t_class *Class, bool v0 = false, bool fT0 = false, bool once0 = false, bool continuousForce0 = false);
+		~pofVisible();
 
 		virtual void tree_draw();
 		
 		static void setup(void);
+		static void initFrame(ofEventArgs & args);
 		
 		bool visible;
 		bool forceTouchable;
+		bool once;
+		bool continuousForce;
+		bool force;
+		bool final_once;
+		bool final_force;
 		t_symbol *layer;
+		static bool FORCE_ONCE;
+		static std::list<pofVisible*> onces;
 };
 
 
