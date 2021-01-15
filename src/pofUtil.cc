faee6ae4d839eca20dc11fe05a2a89b4d12d2583
Antoine Rousseau <_antoine_@metalu.net>
2021-01-15 19:01:02 +0100

    remove Poco dependency from pofUtil's file downloader (and fix some tabs)

--- src/pofUtil.cc
+++ src/pofUtil.cc
@@ -5,6 +5,7 @@
  */
 #include "pofUtil.h"
 #include "ofxZipPass.h"
+#include "ofURLFileLoader.h"
 
 static t_class *pofutil_class;
 static t_symbol *s_download, *s_out, *s_unzip, *s_done, *s_error;
@@ -13,15 +14,15 @@ static t_symbol *s_rmfile,*s_rmdir,*s_mkdir,*s_renamedir,*s_movefile,*s_copyfile
 
 static void *pofutil_new(t_symbol *s, int argc, t_atom *argv)
 {
-    pofUtil* obj = new pofUtil(pofutil_class);
-    
+	pofUtil* obj = new pofUtil(pofutil_class);
+
 	canvas_getargs(&obj->pargc, &obj->pargv);
-    obj->pdcanvas = canvas_getcurrent();
+	obj->pdcanvas = canvas_getcurrent();
 
 	if((argv->a_type == A_SYMBOL)&&(atom_getsymbol(argv) == gensym("sync")))
 		obj->sync = true;
 
-    return (void*) (obj->pdobj);
+	return (void*) (obj->pdobj);
 }
 
 static void pofutil_out(void *x, t_symbol *s, int argc, t_atom *argv)
@@ -36,7 +37,7 @@ static void pofutil_getdir(void *x)
 {
 	pofUtil* px= (pofUtil*)(((PdObject*)x)->parent);
 	t_atom ap;
-	
+
 	SETSYMBOL(&ap, canvas_getdir(px->pdcanvas));
 	outlet_anything(px->m_out1, gensym("getdir"), 1, &ap);
 }
@@ -45,7 +46,7 @@ static void pofutil_getdatapath(void *x)
 {
 	pofUtil* px= (pofUtil*)(((PdObject*)x)->parent);
 	t_atom ap;
-	
+
 	SETSYMBOL(&ap, gensym(ofToDataPath("", true).c_str()));
 	outlet_anything(px->m_out1, gensym("getdatapath"), 1, &ap);
 }
@@ -57,7 +58,7 @@ static void pofutil_time(void *x, t_symbol *s, int argc, t_atom *argv)
 	char *buf;
 	int buflen;
 	t_atom ap;
-	
+
 	binbuf_add(bb, argc, argv);
 	binbuf_gettext(bb, &buf, &buflen);
 
@@ -67,7 +68,7 @@ static void pofutil_time(void *x, t_symbol *s, int argc, t_atom *argv)
 
 	binbuf_free(bb);
 	freebytes(buf, buflen);
-	
+
 	outlet_anything(px->m_out1, gensym("time"), 1, &ap);
 }
 
@@ -103,7 +104,7 @@ static void pofutil_fileinfo(void *x, t_symbol *path)
 
 	SETFLOAT(&at[1], std::filesystem::last_write_time(file) / 86400); // days from 1970
 	SETFLOAT(&at[2], std::filesystem::last_write_time(file) % 86400); // seconds in the day
-    
+
 	if(file.isFile()) {
 		SETFLOAT(&at[3], file.getSize());
 		outlet_anything(px->m_out1, gensym("fileinfo"), 4, at);
@@ -121,17 +122,17 @@ static void pofutil_listdir(void *x, t_symbol *s, int argc, t_atom *argv)
 {
 	pofUtil* px= (pofUtil*)(((PdObject*)x)->parent);
 	t_symbol *s_dir;
-	
+
 	if((argc>0)&&(argv->a_type == A_SYMBOL)) {
 		s_dir = atom_getsymbol(argv); 
 		ofDirectory dir(s_dir->s_name);
 		argc-- ; argv++;
-		
+
 		while((argc>0)&&(argv->a_type == A_SYMBOL)) {
 			dir.allowExt(atom_getsymbol(argv)->s_name);
 			argc-- ; argv++;
 		}
-		
+
 		int n = dir.listDir();
 		dir.sort();
 		t_atom *at = (t_atom *)getbytes((n+1) * sizeof(t_atom) );
@@ -158,7 +159,7 @@ static void pofutil_dirbaseext(void *x, t_symbol *file)
 {
 	pofUtil* px= (pofUtil*)(((PdObject*)x)->parent);
 	t_atom at[3];
-	
+
 	SETSYMBOL(&at[0], gensym(ofFilePath::getEnclosingDirectory(file->s_name).c_str()));
 	SETSYMBOL(&at[1], gensym(ofFilePath::getBaseName(file->s_name).c_str()));
 	SETSYMBOL(&at[2], gensym(ofFilePath::getFileExt(file->s_name).c_str()));
@@ -170,7 +171,7 @@ static void pofutil_fileops(void *x, t_symbol *command, int argc, t_atom *argv, 
 {
 	pofUtil* px= (pofUtil*)(((PdObject*)x)->parent);
 	float errcode = 0;
-	
+
 	t_atom at[4];
 	SETSYMBOL(&at[0], s_out);
 	SETSYMBOL(&at[1], command);
@@ -216,8 +217,8 @@ static void pofutil_fileops(void *x, t_symbol *command, int argc, t_atom *argv, 
 					false,											// bRelativeToData
 					argc>2 ? atom_getfloat(argv+2) != 0 : false))	//overwrite
 				SETSYMBOL(&at[2], s_done);
-        }
-    }
+		}
+	}
 	else if(command == s_copyfile) {
 		if((argc>1) && (argv->a_type == A_SYMBOL) && ((argv+1)->a_type == A_SYMBOL)) {
 			if((ofFile(atom_getsymbol(argv)->s_name) == ofFile(atom_getsymbol(argv+1)->s_name)) // silently skip if same path
@@ -237,31 +238,31 @@ static void pofutil_fileops(void *x, t_symbol *command, int argc, t_atom *argv, 
 //--------------------- sys thread (for asynchronous operations : mkdir rmdir renamedir rmfile...) ----------------
 class pofSysThread : public ofThread {
 	public :
-    void *obj;
-    t_symbol *command;
-    t_binbuf *binbuf;
-    bool running;
-    bool error;
-    
+	void *obj;
+	t_symbol *command;
+	t_binbuf *binbuf;
+	bool running;
+	bool error;
+
 	pofSysThread(void *x, t_symbol *s, int ac, t_atom *av) : 
  		obj(x), command(s), binbuf(NULL), running(false){
 		binbuf = binbuf_new();
 		binbuf_add(binbuf, ac, av);
 	}
-	
+
 	~pofSysThread() {
 		binbuf_free(binbuf);
 	}
-	
+
 	void threadedFunction(){
 		running = true;
 		int argc = binbuf_getnatom(binbuf);
 		t_atom *argv = binbuf_getvec(binbuf);
-		
+
 		pofutil_fileops(obj, command, argc, argv, false);
 		running = false;
 		return;
-    }
+	}
 };
 
 static void pofutil_thread(void *x, t_symbol *s, int argc, t_atom *argv)
@@ -296,15 +297,15 @@ static void pofutil_sync(void *x, t_float sync)
 
 class pofUnzipper : public ofThread {
 	public :
-    void *obj;
-    string file;
-    string path;
-    string passwd;
-    bool running;
-    bool error;
-    
+	void *obj;
+	string file;
+	string path;
+	string passwd;
+	bool running;
+	bool error;
+
 	pofUnzipper(void *x, string _file, string _path, string _passwd) : 
- 		obj(x), file(_file), path(_path), passwd(_passwd), running(false){
+		obj(x), file(_file), path(_path), passwd(_passwd), running(false){
 	}
 	
 	~pofUnzipper() {
@@ -330,150 +331,66 @@ class pofUnzipper : public ofThread {
 		((pofUtil*)obj)->queueToSelfPd(4,at);
 		running = false;
 		return;
-    }
+	}
 };
 
 static void pofutil_unzip(void *x, t_symbol *zipfile, t_symbol *path, t_symbol *password)
 {
 	pofUtil* px= (pofUtil*)(((PdObject*)x)->parent);
 	zipfile = makefilename(zipfile, px->pdcanvas);
-	
+
 	if(px->unzipper != NULL && px->unzipper->running) return;
 	if(px->unzipper) delete px->unzipper;
 	
 	if (!password || !*password->s_name) password = gensym("");
 	if(password->s_name[0]=='@') password = gensym(pofBase::getString(password->s_name + 1).c_str());
-	
+
 	px->unzipper = new pofUnzipper(px, zipfile->s_name, path->s_name, password->s_name);
 	px->unzipper->startThread();
 }
 
 /* FileDownload : */
 static void pofutil_download_tick(void *x);
-#include "Poco/Net/HTTPSession.h"
-#include "Poco/Net/HTTPClientSession.h"
-#include "Poco/Net/HTTPSClientSession.h"
-#include "Poco/Net/HTTPRequest.h"
-#include "Poco/Net/HTTPResponse.h"
-#include "Poco/StreamCopier.h"
-#include "Poco/Path.h"
-#include "Poco/URI.h"
-#include "Poco/Exception.h"
-#include "Poco/URIStreamOpener.h"
-#include "Poco/Net/HTTPStreamFactory.h"
-#include "Poco/Net/HTTPSStreamFactory.h"
-#include "Poco/Net/SSLManager.h"
-#include "Poco/Net/KeyConsoleHandler.h"
-#include "Poco/Net/ConsoleCertificateHandler.h"
-using namespace Poco::Net;
-
-using namespace Poco;
 
 class pofFileDownloader : public ofThread {
 	public :
-    void *obj;
-    string url;
-    string file;
-    bool done;
-    bool error;
-   	string redirection;
-    t_clock *x_clock;
-    ofPtr<HTTPSession> session;
-    
+	void *obj;
+	string url;
+	string file;
+	bool done;
+	bool error;
+	string redirection;
+	t_clock *x_clock;
+
 	pofFileDownloader(void *x, string _url, string _file) : obj(x), url(_url), file(_file), done(false), error(false), redirection("") {
 		x_clock = clock_new(x, (t_method)pofutil_download_tick);
 		clock_delay(x_clock, 500);
 	}
 	
-	~pofFileDownloader() {
+	virtual ~pofFileDownloader() {
 		clock_free(x_clock);
 	}
-	
-	istream *request(URI &uri, HTTPResponse &res)
-	{
-		try {
-			istream * rs;
-			
-			std::string path(uri.getPathAndQuery());
-			if (path.empty()) path = "/";
 
-			HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
-
-			if(uri.getScheme()=="https"){
-				HTTPSClientSession * httpsSession = new HTTPSClientSession(uri.getHost(), uri.getPort());
-				httpsSession->setTimeout(Poco::Timespan(20,0));
-				httpsSession->sendRequest(req);
-				rs = &httpsSession->receiveResponse(res);
-				session = ofPtr<HTTPSession>(httpsSession);
-			}else{
-				HTTPClientSession * httpSession = new HTTPClientSession(uri.getHost(), uri.getPort());
-				httpSession->setTimeout(Poco::Timespan(20,0));
-				httpSession->sendRequest(req);
-				rs = &httpSession->receiveResponse(res);
-				session = ofPtr<HTTPSession>(httpSession);
-			}
-			
-			return rs;
-		} catch (...) { 
-			return NULL;
-		}		
-	}
-	
 	void threadedFunction(){
 		try {
-
-			URI uri(url);
-			HTTPResponse res;
-
-			istream * rs = request(uri, res);
-
-			if(res.getStatus()>=300 && res.getStatus()<400){
-				Poco::URI uri(res.get("Location"));
-				redirection = res.get("Location");
-				done = true;
-				return;
-			}
-			
-			if(rs == NULL) {
-				error = true;
-				ofLogError("pofUtil_download : ") << "NULL istream." ;
-		    	return;
-		    }
-		    
-			ofFile saveTo(file, ofFile::WriteOnly, true);
-			char aux_buffer[1024];
-			rs->read(aux_buffer, 1024);
-			std::streamsize n = rs->gcount();
-			while (n > 0 && isThreadRunning()){
-				// we resize to size+1 initialized to 0 to have a 0 at the end for strings
-				saveTo.write(aux_buffer,n);
-				if (rs->good()){
-					rs->read(aux_buffer, 1024);
-					n = rs->gcount();
-				}
-				else n = 0;
-			}
-			done = true;
+			ofHttpResponse response = ofSaveURLTo(url, file);
+			if (response.status == 200) done = true;
+			else error = true;
 			return;
 		} catch (const Exception& exc) {
-		    //ofLogError("pofUtil_download") << "handleRequest(): "+ exc.displayText();
+			ofLogError("pofUtil_download") << "handleRequest(): "+ exc.displayText();
 			error = true;
-		    return;
-
-		} catch (...) {
-			error = true;
-		    return;
+			return;
 		}
-
 		error = true;
-	    return;
-    }
+		return;
+	}
 
-    float getMb() {
-    	if(ofFile(file).exists())
-    	return (ofFile(file).getSize()/(float)1000000);
-    	else return 0.0;
-    }
+	float getMb() {
+		if(ofFile(file).exists())
+		return (ofFile(file).getSize()/(float)1000000);
+		else return 0.0;
+	}
 };
 
 static void pofutil_download_tick(void *x)
@@ -502,7 +419,7 @@ static void pofutil_download_tick(void *x)
 		SETFLOAT(at + 1, px->fileDownloader->getMb());
 		outlet_anything(px->m_out1, s_download, 2, at);
 		clock_delay(px->fileDownloader->x_clock, 500);
-	}		
+	}
 }
 
 static void pofutil_download(void *x, t_symbol *s, int argc, t_atom *argv)
@@ -510,34 +427,34 @@ static void pofutil_download(void *x, t_symbol *s, int argc, t_atom *argv)
 	pofUtil* px= (pofUtil*)(((PdObject*)x)->parent);
 	t_atom at;
 	t_symbol *url, *path, *pre_url;
-	
+
 	if(argc && (argv->a_type == A_SYMBOL)) {
 		url = atom_getsymbol(argv); 
 		argc-- ; argv++;
 	} else return;
-	
+
 	if(argc && (argv->a_type == A_SYMBOL)) {
 		path = atom_getsymbol(argv); 
 		argc-- ; argv++;
 	} else return;
-	
+
 	if(argc && (argv->a_type == A_SYMBOL)) {
 		pre_url = atom_getsymbol(argv); 
 		argc-- ; argv++;
 	} else pre_url = gensym("");
-	
+
 	if(pre_url->s_name[0] == '@') pre_url = gensym(pofBase::getString(&(pre_url->s_name[1])).c_str());
-	
+
 	string urlStr = string(pre_url->s_name) + string(url->s_name);
-	
+
 	if(px->fileDownloader != NULL) {
 		SETSYMBOL(&at, gensym("error_running"));
 		outlet_anything(px->m_out1, s_download, 1, &at);
 		return;
 	}
-	
+
 	(px->fileDownloader = new pofFileDownloader(x, urlStr, path->s_name))->startThread();
-	
+
 	SETSYMBOL(&at, gensym("start"));
 	outlet_anything(px->m_out1, s_download, 1, &at);
 }
@@ -546,7 +463,7 @@ static void pofutil_abort_download(void *x)
 {
 	pofUtil* px= (pofUtil*)(((PdObject*)x)->parent);
 	t_atom at;
-	
+
 	if(px->fileDownloader) {
 		px->fileDownloader->waitForThread(true);
 		delete px->fileDownloader;
@@ -564,12 +481,10 @@ static void pofutil_free(void *x)
 		px->fileDownloader->waitForThread(true);
 		delete px->fileDownloader;
 	}
-	
 	if(px->unzipper) {
 		px->unzipper->waitForThread(true);
 		delete px->unzipper;
 	}
-	
 	delete px;
 }
 
@@ -581,12 +496,12 @@ void pofUtil::setup(void)
 	s_unzip = gensym("unzip");
 	s_done = gensym("done");
 	s_error = gensym("error");
-	
+
 	s_rmfile =  gensym("rmfile");
 	s_rmdir =  gensym("rmdir");
 	s_mkdir =  gensym("mkdir");
-    s_renamedir =  gensym("renamedir");
-    s_movefile =  gensym("movefile");
+	s_renamedir =  gensym("renamedir");
+	s_movefile =  gensym("movefile");
 	s_copyfile =  gensym("copyfile");
 	
 	pofutil_class = class_new(gensym("pofutil"), (t_newmethod)pofutil_new, (t_method)pofutil_free,
@@ -601,11 +516,11 @@ void pofUtil::setup(void)
 	class_addmethod(pofutil_class, (t_method)pofutil_thread, s_rmfile, A_GIMME, A_NULL);
 	class_addmethod(pofutil_class, (t_method)pofutil_thread, s_rmdir, A_GIMME, A_NULL);
 	class_addmethod(pofutil_class, (t_method)pofutil_thread, s_mkdir, A_GIMME, A_NULL);
-    class_addmethod(pofutil_class, (t_method)pofutil_thread, s_renamedir, A_GIMME, A_NULL);
-    class_addmethod(pofutil_class, (t_method)pofutil_thread, s_movefile, A_GIMME, A_NULL);
+	class_addmethod(pofutil_class, (t_method)pofutil_thread, s_renamedir, A_GIMME, A_NULL);
+	class_addmethod(pofutil_class, (t_method)pofutil_thread, s_movefile, A_GIMME, A_NULL);
 	class_addmethod(pofutil_class, (t_method)pofutil_thread, s_copyfile, A_GIMME, A_NULL);
 	class_addmethod(pofutil_class, (t_method)pofutil_sync, gensym("sync"), A_FLOAT, A_NULL);
-	
+
 	class_addmethod(pofutil_class, (t_method)pofutil_listdir, gensym("listdir"), A_GIMME, A_NULL);
 	class_addmethod(pofutil_class, (t_method)pofutil_exists, gensym("exists"), A_SYMBOL, A_NULL);
 	class_addmethod(pofutil_class, (t_method)pofutil_fileinfo, gensym("fileinfo"), A_SYMBOL, A_NULL);
