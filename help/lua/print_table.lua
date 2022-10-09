f9aefc6b3d17fc964db00e5bab8a9f420a4358be
Antoine Rousseau <_antoine_@metalu.net>
2022-10-09 22:59:41 +0200

    poflua: continue work on help patch

--- help/lua/print_table.lua
+++ help/lua/print_table.lua
@@ -1,12 +1,22 @@
 function M:print_table(nt, ...)
-	local arg = {...}
-	local t = nt
-	t = _G[nt]
+	local args = {...}
+	local t = _G[nt]
+	if nt == "self" then t = self end
+	local res = {}
 
-	for _,v in ipairs(arg) do t=t[v] nt = nt.."."..v end
+	for _,v in ipairs(args) do t=t[v] nt = nt.."."..v end
+
 	if not t then print("no such table") return end
+
 	for k, v in pairs(t) do
-	print(nt .."."..tostring(k))
+		table.insert(res, k)
+	end
+
+	table.sort(res)
+
+	print("\ncontent", "of", nt, ":")
+	for _, v in ipairs(res) do
+		print(nt .."."..tostring(v))
 	end
 end
 
